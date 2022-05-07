fd3f5ff584eaaa0bc44edd99172c09eb63442fc3
MrDecter <59817907+MrDecter@users.noreply.github.com>
2022-05-07 12:13:03 +0300

    Update main.py
    Исправление имен переменных и т.д.

--- main.py
+++ main.py
@@ -12,25 +12,34 @@ sql.execute("""CREATE TABLE IF NOT EXISTS AllItem (
     )""")
 db.commit()
 
+"""
+
+Провести оптимизацию:
+-Случайные нажатия, 
+-Отработка ошибок
+-И наконец то убрать лишние else 
+"""
+
 
 # Функции
 
-# Расчет маржи
-# Добавление кол-во товара
 def add_pieces(code, pieces):
-    item = sql.execute(f'SELECT * FROM AllItem WHERE code = "{code}"').fetchone()
-    sum = int(item[2]) + int(pieces)
-    sql.execute(f'UPDATE AllItem SET pieces = "{sum}" WHERE code = "{code}"')
+    """Функция добавление колличества товара, если он уже есть"""
+
+    item = sql.execute('SELECT * FROM AllItem WHERE code = ?', (code,)).fetchone()
+    sum_item = int(item[2]) + int(pieces)
+    sql.execute('UPDATE AllItem SET pieces = ? WHERE code = ?', (sum_item, code))
     print('Кол-во товара изменено')
     view_table(f"{code}")
 
 
-# Добавление товара
 def add_item(code, item, pieces, price):
+    """Функция добавление товара в SQL"""
+
     Up_item = item.upper()
-    sql.execute(f"SELECT code FROM AllItem WHERE code = '{code}'")
+    sql.execute("SELECT code FROM AllItem WHERE code = ?", (code,))
     if sql.fetchone() is None:
-        sql.execute(f'INSERT INTO AllItem VALUES(?,?,?,?)', (code, Up_item, pieces, price))
+        sql.execute('INSERT INTO AllItem VALUES(?,?,?,?)', (code, Up_item, pieces, price))
         db.commit()
         print("Товар добавлен!")
         view_table(f"{code}")
@@ -50,92 +59,67 @@ def add_item(code, item, pieces, price):
     menu()
 
 
-# Удаление товара
 def delete_item(code):
-    sql.execute(f"DELETE FROM AllItem WHERE code = '{code}'")
+    """Функция удаление товара из SQL по его коду"""
+
+    sql.execute(f"DELETE FROM AllItem WHERE code = ?", (code,))
     print("Товар удален!")
     print("Возврат в меню")
     time.sleep(1)
     menu()
 
 
-# Корректное отображение остатков товара
-def view_table(item_code):
-    table = sql.execute(f"SELECT * FROM AllItem WHERE code = '{item_code}'").fetchall()
+def view_table(words, flag=True, item_code=0):
+    """Функция отображение товаров из SQL"""
+
+    if flag:
+        table = sql.execute("SELECT * FROM AllItem WHERE code = ?", (item_code,)).fetchall()
+    else:
+        table = sql.execute("SELECT * FROM AllItem ORDER BY code").fetchall()
     q = 0
     i = 0
+    p = "-----------"
     while q != len(table):
+        print(p)
         while i != 4:
-            if i == 0:
-                print('Код: ' + str(table[q][i]))
-                i += 1
-            elif i == 1:
-                print('Название: ' + str(table[q][i]))
-                i += 1
-            elif i == 2:
-                print('Кол-во: ' + str(table[q][i]))
-                i += 1
-            elif i == 3:
-                print('Стоимость за шт: ' + str(table[q][i]) + 'р.')
-                i += 1
-            else:
-                print("Error")
-        q += 1
-    print("\nВозврат в меню")
-    time.sleep(1)
-    menu()
-
-
-# Корректное отображение всей таблицы
-def view_all_table():
-    table = sql.execute("SELECT * FROM AllItem ORDER BY code").fetchall()
-    q = 0
-    i = 0
-    while q != len(table):
-        print("---------------")
-        while i != 4:
-            if i == 0:
-                print('Код: ' + str(table[q][i]))
-                i += 1
-            elif i == 1:
-                print('Название: ' + str(table[q][i]))
-                i += 1
-            elif i == 2:
-                print('Кол-во: ' + str(table[q][i]))
-                i += 1
-            elif i == 3:
-                print('Цена: ' + str(table[q][i]) + 'р.')
-                i += 1
-            else:
-                print("Error")
-        q += 1
+            print(words[i] + str(table[q][i]))
+            i += 1
         i -= 4
+        q += 1
     print("\nВозврат в меню")
     time.sleep(1)
     menu()
 
 
-# Меню
 def menu():
-    logist = int(input(
+    """Функция управления"""
+
+    menu_manage = int(input(
         "1. Добавить товар\n2. Посмотреть все товары\n3. Посмотреть остатки товара\n4. Удалить товар\n5. Выход\n"))
-    if logist == 1:
-        menu_code = int(input("Введите код товара: "))
-        menu_item = input("Введите название товара: ")
-        menu_pic = int(input("Введите кол-во товара: "))
-        menu_price = int(input("Введите стоимость товара за шт: "))
-        add_item(menu_code, menu_item, menu_pic, menu_price)
-    elif logist == 2:
-        view_all_table()
-    elif logist == 3:
-        menu_name = int(input('Введите код товара: '))
-        view_table(menu_name)
-    elif logist == 4:
+    if menu_manage == 1:
+        sql_code = int(input("Введите код товара: "))
+        sql_name = input("Введите название товара: ")
+        sql_pieces = int(input("Введите кол-во товара: "))
+        sql_price = int(input("Введите стоимость товара за шт: "))
+        add_item(sql_code, sql_name, sql_pieces, sql_price)
+
+    elif menu_manage == 2:
+        words = ["Код: ", "Название: ", "Кол-во: ", "Цена за шт: "]
+        view_table(words, flag=False)
+
+    elif menu_manage == 3:
+        sql_code = int(input('Введите код товара: '))
+        words = ["Код: ", "Название: ", "Кол-во: ", "Цена за шт: "]
+        view_table(words, item_code=sql_code)
+
+    elif menu_manage == 4:
         menu_del = input('Введите код удаляемого товара: ')
         delete_item(menu_del)
-    elif logist == 5:
+
+    elif menu_manage == 5:
         print("Всего доброго!")
         time.sleep(3)
+
     else:
         print("Error")
 
@@ -143,3 +127,4 @@ def menu():
 # Запуск
 print("Добро пожаловать!")
 menu()
+
